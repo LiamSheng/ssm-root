be3c6aeeace8028b6b5c13b959ea5d4fd8cdbf09
Linzi <linzi-s@outlook.com>
2024-10-02 22:08:23 +0800

    Test addming components.
    Test Spring’s method of adding components.

--- spring-01-ioc/src/main/java/linzi/ssm/spring01ioc/Spring01IocApplication.java
+++ spring-01-ioc/src/main/java/linzi/ssm/spring01ioc/Spring01IocApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 
+import java.util.Map;
+
 /**
  * 这个是主程序类
  */
@@ -21,14 +23,25 @@ public class Spring01IocApplication {
         System.out.println("<--------------------------------->");
 
         //3. 获取容器中所有组件的名字。
-        String[] names = ioc.getBeanDefinitionNames();
-        for (String name : names) {
-            System.out.println(name);
-        }
+//        String[] names = ioc.getBeanDefinitionNames();
+//        for (String name : names) {
+//            System.out.println(name);
+//        }
 
+        //5. 获取容器中的组建对象。
+        // 组件的四大特性：名字，类型，对象和作用域。
+        //  名字：默认是方法名，但是可以使用 @Bean("新名字") 修改，要求全局唯一。
+        //      重复名字的组件，容器只添加第一个对象。
+
+        //5.1 按照组件的名字获取对象，需要强转。
+        Person linzi = (Person) ioc.getBean("linzi");
+        //5.2 按照组件的类型获取对象，不需要强转但是要保证是唯一的。
+        Person linzi2 = ioc.getBean(Person.class);
+        //5.3 按照类型获取一类对象。
+        Map<String, Person> persons = ioc.getBeansOfType(Person.class);
     }
 
-    @Bean
+    @Bean("linzi")
     //4. 在容器中注册一个自己的组件。
     public Person person() {
         Person person = new Person();
