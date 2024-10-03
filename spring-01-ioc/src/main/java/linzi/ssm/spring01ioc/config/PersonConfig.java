13f3fdb3aa6b4988cea316aabe733580f6a82bf1
Linzi <linzi-s@outlook.com>
2024-10-03 15:27:19 +0800

    Verify Singleton Scope of Components and @Configuration Functionality
    - Added tests to confirm that components are singletons within the Spring container.
    - Verified the functionality of the @Configuration annotation in defining and managing bean instances.

---
+++ spring-01-ioc/src/main/java/linzi/ssm/spring01ioc/config/PersonConfig.java
@@ -0,0 +1,30 @@
+package linzi.ssm.spring01ioc.config;
+
+import linzi.ssm.spring01ioc.bean.Person;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration // 告诉spring容器，这是一个配置类。
+public class PersonConfig {
+
+    @Bean("linzi")
+    //4. 在容器中注册一个自己的组件。
+    public Person linzi() {
+        Person person = new Person();
+        person.setName("");
+        person.setAge(0);
+        person.setGender("");
+        return person;
+    }
+
+    @Bean("linzi2")
+    //4. 在容器中注册一个自己的组件。
+    public Person linzi2() {
+        Person person = new Person();
+        person.setName("");
+        person.setAge(0);
+        person.setGender("");
+        return person;
+    }
+
+}
