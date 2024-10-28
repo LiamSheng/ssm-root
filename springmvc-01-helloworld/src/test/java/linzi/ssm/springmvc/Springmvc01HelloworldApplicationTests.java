2d2f650b4a01bc2dc25c35cc94b39d04524ec7fc
Linzi <linzi-s@outlook.com>
2024-10-28 14:58:04 +0800

    Added RequestMapping limitations in SpringMVC Controller
    - Limited methods to POST and GET.
    - Restricted params to include 'username', 'age=18', and 'gender!=male'.
    - Added header restrictions with 'valid-headers'.
    - Set consumes to application/json.
    - Set produces to text/html.

---
+++ springmvc-01-helloworld/src/test/java/linzi/ssm/springmvc/Springmvc01HelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package linzi.ssm.springmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springmvc01HelloworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
