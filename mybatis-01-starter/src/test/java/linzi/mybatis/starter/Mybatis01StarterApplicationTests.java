60dd4abe495c07913e993b665ac50f9717fd0e92
Linzi <linzi-s@outlook.com>
2024-11-09 16:25:56 +0800

    Enhance CRUD methods and enable camelCase mapping with MyBatis
    1. Enhanced the CRUD methods in the project to ensure comprehensive data operations.
       - Added missing CRUD methods to handle Create, Read, Update, and Delete operations effectively.
    
    2. Configured MyBatis to automatically map underscore-separated column names to camelCase property names.
       - Enabled configuration `mybatis.configuration.map-underscore-to-camel-case=true` to streamline mapping between database columns and Java properties.
    
    Benefits:
    - Improved data interaction capabilities with fully implemented CRUD operations.
    - Simplified mapping between database fields and Java object properties, enhancing code readability and maintainability.

--- mybatis-01-starter/src/test/java/linzi/mybatis/starter/Mybatis01StarterApplicationTests.java
+++ mybatis-01-starter/src/test/java/linzi/mybatis/starter/Mybatis01StarterApplicationTests.java
@@ -1,19 +1,67 @@
 package linzi.mybatis.starter;
 
-import linzi.mybatis.starter.dao.EmpMapper;
+import linzi.mybatis.starter.bean.Emp;
+import linzi.mybatis.starter.mapper.EmpMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
 @SpringBootTest
 class Mybatis01StarterApplicationTests {
 
-    @Autowired
+    @Autowired // 容器中是 MyBatis 为每个 Mapper 接口创建的代理对象.
     private EmpMapper empMapper;
 
+    /**
+     * 测试 MyBatis 获取员工信息.
+     */
     @Test
     void testMybatisSearch() {
         System.out.println(empMapper.getEmpById(1));
+    }
+
+    /**
+     * 测试 MyBatis 插入员工信息.
+     */
+    @Test
+    void testMybatisInsert() {
+        Emp emp = new Emp();
+        emp.setEmpName("LZsS");
+        emp.setAge(18);
+        emp.setEmpSalary(40000.00);
+        empMapper.addEmp(emp);
+        System.out.println("新添加的emp的id是: " + emp.getId());
+    }
+
+    /**
+     * 测试 MyBatis 更新员工信息.
+     */
+    @Test
+    void testMybatisUpdate() {
+        Emp emp = empMapper.getEmpById(1);
+        emp.setEmpName(emp.getEmpName() + "updated");
+        empMapper.updateEmp(emp);
+    }
+
+    /**
+     * 测试 MyBatis 删除员工信息.
+     */
+    @Test
+    void testMybatisDelete() {
+        empMapper.deleteEmpById(4);
+    }
+
+    /**
+     * 测试 MyBatis 获取所有员工信息.
+     */
+    @Test
+    void testMybatisFindAll() {
+        List<Emp> emps = empMapper.getAllEmp();
+        for (Emp emp : emps) {
+            System.out.println(emp);
+        }
     }
 
 }
