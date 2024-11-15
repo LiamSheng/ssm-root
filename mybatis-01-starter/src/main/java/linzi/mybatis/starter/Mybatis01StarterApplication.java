819301d37a9b15c1b40608d9100861048220ccb1
Linzi <linzi-s@outlook.com>
2024-11-15 21:33:46 +0800

    Implement dynamic SQL techniques in MyBatis for flexible query generation
    - Integrated dynamic SQL capabilities using MyBatis.
    - Utilized 'where', 'trim', 'if', and 'choose' tags to conditionally generate SQL based on runtime parameters.
    - Employed 'set' tag for dynamic updates.
    - Applied 'foreach' tag for batch operations (insert, update, delete).
    - Enhanced flexibility and performance in SQL query generation and execution.

--- mybatis-01-starter/src/main/java/linzi/mybatis/starter/Mybatis01StarterApplication.java
+++ mybatis-01-starter/src/main/java/linzi/mybatis/starter/Mybatis01StarterApplication.java
@@ -2,8 +2,10 @@ package linzi.mybatis.starter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class Mybatis01StarterApplication {
 
     public static void main(String[] args) {
